dbb8db8faff6e13a5a3e43cecbfa4dfd8e253da2
VAANI1 <85991013+VAANI1@users.noreply.github.com>
2022-07-15 18:01:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300 , 300);
+    canvas.position(110, 250);
+    canvas.center();  
+    video = createCapture(VIDEO);
+    video.hide(); 
+}
+
+function draw(){
+    image(video, 0, 0, 640, 480);
+}
+
+function take_snapshot(){
+    save('myLipstickImage.png');    
+}
+
